fa0f375cd6f1b03274b05a59bc90c7f3c51dcc97
Bob McAleavey <bobmcaleavey@gmail.com>
2021-11-22 07:23:24 -0500

    remove - assets & prerendered middleware

--- server.express.js
+++ server.express.js
@@ -2,15 +2,9 @@
 import compression from 'compression';
 import express from 'express';
 import http from 'http';
-import path from 'path';
 import { Server } from 'socket.io';
-import { fileURLToPath, URL, URLSearchParams } from "url";
 import { assetsMiddleware, prerenderedMiddleware, kitMiddleware } from './build/middlewares.js';
 import { serverEvents, socketEvents } from './src/lib/socketio/index.js';
-
-// initiate path variables
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
 
 // initiate app
 const app = express();
@@ -26,8 +20,6 @@ serverEvents(io, socketEvents);
 
 // add middlewares
 app.use(compression());
-app.use('/', express.static(path.join(__dirname, 'assets')));
-app.use('/', express.static(path.join(__dirname, 'prerendered')));
 app.use('*', assetsMiddleware, prerenderedMiddleware, kitMiddleware);
 
 // server listen
